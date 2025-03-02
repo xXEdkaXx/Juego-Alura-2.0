93736a74656a128780bd808f21b71a730969a534
Edka <118858852+xXEdkaXx@users.noreply.github.com>
2025-03-01 18:53:57 -0600

    Agregar primeras funciones al js

---
+++ app.js
@@ -0,0 +1,34 @@
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 0;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p','Acertaste el número!');
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento ('p', 'El número secreto es menor');
+        } else {
+            asignarTextoElemento ('p', 'El número secreto es mayor');
+        }
+    }
+    return;
+}
+
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10)+1;
+
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p',`Indica un número del 1 al 10`);
